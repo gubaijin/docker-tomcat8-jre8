47e580da33c7084f6565ad49c0c98c8593152e6c
Kevin GU <gubaijing2004@163.com>
2017-02-16 12:07:12 +0800

    Update setShell.sh

--- setShell.sh
+++ setShell.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-if [[ "$JAVA_OPTS" != *-Djava.security.egd=* ]]; then
-    JAVA_OPTS="$JAVA_OPTS -Djava.security.egd=file:/dev/./urandom"
+if[["$JAVA_OPTS"!=*-Djava.security.egd=*]];then JAVA_OPTS="$JAVA_OPTS -Djava.security.egd=file:/dev/./urandom"
 fi
